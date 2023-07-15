20e5477a8bd8bf4f7d2d3eeb3becd8fed5317641
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-16 00:58:42 +0500

    create other components

---
+++ src/components/content/PageContent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './pagecontent.module.css';
+
+const PageContent: React.FC = () => {
+  return <div className={styles.container}>PageContent</div>;
+};
+
+export default PageContent;
---
+++ src/components/sidemenu/SideMenu.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './sidemenu.module.css';
+
+const SideMenu: React.FC = () => {
+  return <div className={styles.container}>SideMenu</div>;
+};
+
+export default SideMenu;
